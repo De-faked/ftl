21c0b43a9570505101f5a2dc8ade8b9fc7121bb1
De-faked <130953528+De-faked@users.noreply.github.com>
2026-01-08 23:56:16 +0300

    Update LanguageContext.tsx

--- contexts/LanguageContext.tsx
+++ contexts/LanguageContext.tsx
@@ -14,7 +14,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export const LanguageProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [language, setLanguage] = useState<Language>('en');
+  const [language, setLanguage] = useState<Language>('id');
 
   useEffect(() => {
     const dir = language === 'ar' ? 'rtl' : 'ltr';
